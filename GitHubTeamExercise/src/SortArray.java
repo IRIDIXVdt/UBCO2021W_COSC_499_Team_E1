c246677ee883414777f3295cbc126592e1a74db2
jmz456123 <970849953@qq.com>
2021-09-18 13:08:24 +0800

    Update
    completed feature 1 and the part of main

---
+++ GitHubTeamExercise/src/SortArray.java
@@ -0,0 +1,31 @@
+public class SortArray {
+    public static void main(String[] args) {
+		
+		int [] testArray = {3,4,1,2};
+		
+        int [] arr = sortIntArrayDesc(testArray);
+        System.out.print("sort {3,4,1,2} Desc: ");
+        for(int item:arr){
+            System.out.print(item+" ");
+        }
+	}
+
+
+
+
+    public static int[] sortIntArrayDesc(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i;j > 0;j--){
+                if (arr[j] <= arr[j-1]){
+                    break;
+                }else{
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+       
+        return arr;
+    }
+}
